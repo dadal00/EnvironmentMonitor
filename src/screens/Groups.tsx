b1828631bffa85549c40a11b8f4d8375a8fb8f47
dadal00 <dylanadal.00@gmail.com>
2024-07-30 15:42:24 -0700

    group screen progress

--- src/screens/Groups.tsx
+++ src/screens/Groups.tsx
@@ -29,28 +29,144 @@ const GroupsScreen = () => {
                         </View>
                         <View style={styles.group_holder}>
                             <Text style={styles.group_name}>Group #1</Text>
-                            <View style={styles.user_icon}>
-                                <Text style={styles.user_icon_text}>AW</Text>
+                            <View style={styles.user_icons_holder}>
+                                <View style={styles.user_icon3}>
+                                    <Text style={styles.user_icon_text3}>ZA</Text>
+                                </View>
+                                <View style={styles.user_icon2}>
+                                    <Text style={styles.user_icon_text2}>AM</Text>
+                                </View>
+                                <View style={styles.user_icon}>
+                                    <Text style={styles.user_icon_text}>AW</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.group}>
+                        <View style={styles.shadow_image}>
+                            <Image
+                                source={{ uri: 'group_icon' }}
+                                style={styles.group_icon}
+                                resizeMode="contain"
+                            />
+                        </View>
+                        <View style={styles.group_holder}>
+                            <Text style={styles.group_name}>Group #1</Text>
+                            <View style={styles.user_icons_holder}>
+                                <View style={styles.user_icon3}>
+                                    <Text style={styles.user_icon_text3}>ZA</Text>
+                                </View>
+                                <View style={styles.user_icon2}>
+                                    <Text style={styles.user_icon_text2}>AM</Text>
+                                </View>
+                                <View style={styles.user_icon}>
+                                    <Text style={styles.user_icon_text}>AW</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.group}>
+                        <View style={styles.shadow_image}>
+                            <Image
+                                source={{ uri: 'group_icon' }}
+                                style={styles.group_icon}
+                                resizeMode="contain"
+                            />
+                        </View>
+                        <View style={styles.group_holder}>
+                            <Text style={styles.group_name}>Group #1</Text>
+                            <View style={styles.user_icons_holder}>
+                                <View style={styles.user_icon3}>
+                                    <Text style={styles.user_icon_text3}>ZA</Text>
+                                </View>
+                                <View style={styles.user_icon2}>
+                                    <Text style={styles.user_icon_text2}>AM</Text>
+                                </View>
+                                <View style={styles.user_icon}>
+                                    <Text style={styles.user_icon_text}>AW</Text>
+                                </View>
                             </View>
                         </View>
                     </View>
                 </ScrollView>
+            </View>
+            <View style={styles.new_group}>
+                <Text style={styles.plus_sign}>+</Text>
             </View>
         </View>
      );
 };
 
 const styles = StyleSheet.create({
+    plus_sign: {
+        fontFamily: 'DMSans-Medium',
+        fontSize: SCREEN_WIDTH * 0.175,
+    },
+    new_group: {
+        width: SCREEN_WIDTH * 0.9 * 0.86 * 0.985,
+        backgroundColor: '#F8F8F8',
+        borderRadius: SCREEN_WIDTH * 0.0625,
+        padding: SCREEN_WIDTH * 0.028,
+        alignSelf: 'center',
+        marginRight: -SCREEN_WIDTH * 0.02,
+        ...Platform.select({
+            ios: {
+                shadowColor: '#B4B2A6',
+                shadowOffset: { width: SCREEN_WIDTH * 0.015, height: SCREEN_WIDTH * 0.015 },
+                shadowOpacity: 1,
+                shadowRadius: 0, 
+            }
+        }), 
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    user_icons_holder: {
+        flexDirection: 'row-reverse',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        // backgroundColor: 'blue',
+    },
     user_icon_text: {
-
+        fontFamily: 'SpaceGrotesk-SemiBold',
+        color:'#F8F8F8',
+        fontSize: SCREEN_WIDTH * 0.03,
     },
     user_icon: {
         backgroundColor: '#686868',
-        borderRadius: SCREEN_WIDTH * 0.05,
-        width: SCREEN_WIDTH * 0.1,
-        height: SCREEN_WIDTH * 0.1,
+        borderRadius: SCREEN_WIDTH * 0.035,
+        width: SCREEN_WIDTH * 0.07,
+        height: SCREEN_WIDTH * 0.07,
         justifyContent: 'center',
         alignItems: 'center',
+        marginRight: -SCREEN_WIDTH * 0.0235,
+    },
+    user_icon_text2: {
+        fontFamily: 'SpaceGrotesk-SemiBold',
+        color:'#F8F8F8',
+        fontSize: SCREEN_WIDTH * 0.03,
+    },
+    user_icon2: {
+        backgroundColor: '#BFBEB5',
+        borderRadius: SCREEN_WIDTH * 0.035,
+        width: SCREEN_WIDTH * 0.07,
+        height: SCREEN_WIDTH * 0.07,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: -SCREEN_WIDTH * 0.0235,
+    },
+    user_icon_text3: {
+        fontFamily: 'SpaceGrotesk-SemiBold',
+        color:'#2F2F2F',
+        fontSize: SCREEN_WIDTH * 0.03,
+    },
+    user_icon3: {
+        backgroundColor: '#D2D2D2',
+        borderRadius: SCREEN_WIDTH * 0.035,
+        width: SCREEN_WIDTH * 0.07,
+        height: SCREEN_WIDTH * 0.07,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: -SCREEN_WIDTH * 0.0235,
     },
     group_name: {
         fontFamily: 'DMSans-Medium',
@@ -79,18 +195,18 @@ const styles = StyleSheet.create({
         // borderWidth: SCREEN_WIDTH * 0.01,
     },
     group: {
-        marginBottom: SCREEN_WIDTH * 0.03,
+        marginBottom: SCREEN_WIDTH * 0.0475,
         width: '98.5%',
         flexGrow: 1,
         backgroundColor: '#F8F8F8',
-        borderRadius: SCREEN_WIDTH * 0.05,
+        borderRadius: SCREEN_WIDTH * 0.0625,
         padding: SCREEN_WIDTH * 0.03,
         flexDirection: 'row',
         // paddingHorizontal: SCREEN_WIDTH * 0.06,
         ...Platform.select({
             ios: {
                 shadowColor: '#B4B2A6',
-                shadowOffset: { width: SCREEN_WIDTH * 0.01, height: SCREEN_WIDTH * 0.01 },
+                shadowOffset: { width: SCREEN_WIDTH * 0.015, height: SCREEN_WIDTH * 0.015 },
                 shadowOpacity: 1,
                 shadowRadius: 0, 
             }
@@ -98,10 +214,11 @@ const styles = StyleSheet.create({
     },
     middle_container: {
         width: '86%',
-        height: '54%',
-        backgroundColor: 'white',
+        height: '56%',
+        // backgroundColor: 'white',
         alignSelf: 'center',
         marginRight: -SCREEN_WIDTH * 0.02,
+        marginBottom: SCREEN_WIDTH * 0.015,
     },
     title: {
         fontSize: SCREEN_WIDTH * 0.095,
